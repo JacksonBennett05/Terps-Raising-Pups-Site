53c2c67ce5a92f307229762571b369b641237b3d
JacksonBennett05 <jcb263@gmail.com>
2026-03-31 21:54:05 -0400

    Initial Puppies

--- src/pages/OurPups.jsx
+++ src/pages/OurPups.jsx
@@ -1,11 +1,47 @@
+import './OurPups.css'
+
+
+
+
+
 function OurPups() {
+  const dogs = [
+    {
+     name: "Joey",
+     image: "/joey.png",
+     description: "Joey is currently in training and learning the foundations for future service work.",
+  
+    }, 
+    {
+      name: "Hope",
+      image: "/hope.png",
+      description: "Hope is working on obedience, socialization, and confidence in public settings.",
+    },
+    {
+      name: "Tricky",
+      image: "/tricky.png",
+      description: "Tricky is developing the skills needed to move forward in her service dog journey.",
+    },
+  ]; 
+
   return (
     <main className="started">
       <h1>Our Pups</h1>
-      <p>Our dogs tiy see on campus every day who are currently in training</p>
-    </main>
-  )
+      <p>Our dogs you see on campus every day who are currently in training</p>
 
+      <div className="dogs-grid">
+        {dogs.map((dog) => (
+          <div className="dog-card" key={dog.name}>
+            <img src={dog.image} alt={dog.name} className="dog-image" />
+            <div className="dog-info">
+              <h3>{dog.name}</h3>
+              <p>{dog.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
 
-export default OurPups
+export default OurPups;
